55fab195560b4ea476d16f366f16c9ad11f0855f
h-mejiac <hmejiac02@gmail.com>
2021-03-28 21:13:47 -0600

    modelo usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+    constructor(
+        public _id: String,
+        public empleado: String,
+        public nombre: String,
+        public menorocho: Boolean,
+        public mayornueve: Boolean,
+        public hijos: Number,
+    ) {}
+}
